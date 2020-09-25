4a07758bfa4bd1868538f0a9406e900f10743742
Jules Fouchy <jules.fouchy@ntymail.com>
2020-09-25 22:23:51 +0200

    Alignment :3

--- src/components/Theme.ts
+++ src/components/Theme.ts
@@ -2,9 +2,9 @@ import h from '../framework/h'
 
 export default (theme: String, day: number) => {
     const date = new Date()
-    const startDate = new Date(2020, 9, 1, 0, 0)
-    const endDate   = new Date(2020, 10, 1, 0, 0)
     const today = date.getDate()
+    const startDate = new Date(2020, 9,  1, 0, 0)
+    const endDate   = new Date(2020, 10, 1, 0, 0)
 
     return h( 'p', 
     {
